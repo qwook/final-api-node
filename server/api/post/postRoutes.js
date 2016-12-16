f78539b10ace8bfb0b1a350ce9d59222b93d709e
qwook <henryquoctran@gmail.com>
2016-12-16 13:32:58 -0800

    create with categories and populate get with categories

--- server/api/post/postRoutes.js
+++ server/api/post/postRoutes.js
@@ -7,17 +7,24 @@ var User = require('../user/userModel.js');
 // for building
 router.route('/')
   .get(function(req, res){
-    Post.find({}, function(err, posts) {
-      res.send({success: true, posts: posts});
-    })
+    Post
+      .find({})
+      .populate("categories")
+      .exec(function(err, posts) {
+        res.send({success: true, posts: posts});
+      });
   })
   .post(function(req, res, next) {
     User.findOne({username: req.body.author}, function(err, user) {
       if (err) return next(err);
       if (!user) return next("No such user");
 
-      Category.find({name: req.body.categories || []}, function(err, cats) {
-        if (err) return next(err);
+      Category.find({$or: req.body.categories.map(function(name) {
+        return {name: name}
+      })}, function(err, cats) {
+        if (req.body.categories.length == 0) cats = [];
+        if (err && req.body.categories.length != 0) return next(err);
+        if (cats.length != req.body.categories.length) return next("Couldn't find all categories.");
 
         Post.create({
           title: req.body.title,
